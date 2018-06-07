7873f90abc2ca391cf5add0c84ec29764d46a24e
Brian Sangermano <briansangermano@gmail.com>
2018-06-07 11:11:18 -0300

    project refactor

---
+++ clase-2018-06-07/js/app.js
@@ -0,0 +1,11 @@
+console.log('Hola');
+
+var play = function () {
+  var cells = Board.getCells();
+  Board.updateCells(cells);
+}
+
+window.onload = function () {
+  var btnPlay = Board.getPlayButton();
+  btnPlay.onclick = play; 
+}
