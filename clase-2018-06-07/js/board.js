00199a2c0f3dd8b481e446fe4ea86ea8409e3783
Brian Sangermano <briansangermano@gmail.com>
2018-06-14 08:51:06 -0300

    dynamic cells

--- clase-2018-06-07/js/board.js
+++ clase-2018-06-07/js/board.js
@@ -1,11 +1,23 @@
 var Board = {
   rows: 5,
   colums: 5,
+  getBoard: function () {
+    return document.getElementsByClassName('board')[0];
+  },
   getPlayButton: function () {
      return document.getElementById('play');
   },
   getCells: function () {
     return document.getElementsByClassName('alive');
+  },
+  createCells: function () {
+    var board = Board.getBoard();
+    var html = '';
+    var cellCount = Board.rows * Board.colums;
+    for (var i = 0; i < (Board.rows*Board.colums); i++) {
+      html += '<li></li>';
+    }
+    board.innerHTML = html;
   },
   updateCells: function (cells) {
     for (var i = 0; i < cells.length; i++) {
